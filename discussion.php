d2eaac6bc80a9370f115f91623409dfddf780f5b
marie.clerc@laplateforme.io <marie.clerc@laplateforme.io>
2020-11-23 10:57:47 +0100

    fichiers de base

---
+++ discussion.php
@@ -0,0 +1,5 @@
+Sur cette page, les utilisateurs connectés peuvent voir l’ensemble des
+messages dans un fil de discussion. En dessous du fil de discussion se
+trouvent un champs contenant le message et un bouton permettant de
+l’envoyer. Les utilisateurs non connectés souhaitant accéder à cette page
+sont redirigés vers la page de connexion.
---
+++ inscription.php
@@ -0,0 +1,4 @@
+Le formulaire doit contenir l’ensemble des champs présents dans la table
+“utilisateurs” (sauf “id”) ainsi qu’une confirmation de mot de passe. Dès
+qu’un utilisateur remplit ce formulaire, les données sont insérées dans la
+base de données et l’utilisateur est redirigé vers la page de connexion.
